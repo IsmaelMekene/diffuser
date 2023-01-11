f11c1e9d7d45076eee7ae36bd2b99dee267816ab
mbaye <mbaye.diongue@etu.emse.fr>
2023-01-11 10:07:04 +0100

    update sequence_pushing.task file

--- diffuser/datasets/sequence_pushing_task.py
+++ diffuser/datasets/sequence_pushing_task.py
@@ -5,10 +5,10 @@ import pdb
 import h5py
 import torchvision.transforms as transforms
 
-from .preprocessing import get_preprocess_fn
-from .d4rl import load_environment, sequence_dataset, sequence_dataset_pushv0
-from .normalization import DatasetNormalizer, DatasetNormalizerPush
-from .buffer import ReplayBuffer
+from diffuser.datasets.preprocessing import get_preprocess_fn
+from diffuser.datasets.d4rl import load_environment
+#from diffuser.datasets.normalization import DatasetNormalizer, DatasetNormalizerPush
+#from diffuser.datasets.buffer import ReplayBuffer
 import os
 
 Batch = namedtuple('Batch', 'trajectories conditions')
@@ -42,74 +42,21 @@ class SequenceDatasetPush(torch.utils.data.Dataset):
         self.max_path_length = max_path_length
         self.use_padding = use_padding
         
-        
-        itr = sequence_dataset_pushv0(data_dir,preprocess_fn=self.preprocess_fn,
-                        obs_type=obs_type, max_path_length=max_path_length)
-
-        #itr = sequence_dataset(env, self.preprocess_fn)
-
-        fields = ReplayBuffer(max_n_episodes, max_path_length, termination_penalty)
-        for i, episode in enumerate(itr):
-           fields.add_path(episode)
-        fields.finalize()
-
-        self.normalizer = DatasetNormalizerPush(fields, normalizer, path_lengths=fields['path_lengths'])
-        
-        self.indices = self.make_indices(fields.path_lengths, horizon)
-
-        self.observation_dim = next(itr)['observations'][0].shape #fields.observations.shape[-1]
-        self.action_dim = fields.actions.shape[-1]
-        self.fields = fields
-        self.n_episodes = len(os.listdir(data_dir)) #fields.n_episodes #
-        self.path_lengths = fields.path_lengths
-        self.normalize()
-
-        print(fields)
-        # shapes = {key: val.shape for key, val in self.fields.items()}
-        # print(f'[ datasets/mujoco ] Dataset fields: {shapes}')
-
+        entries = os.listdir(data_dir)
+        self.entries = entries # every file in entries contains a path
+        h5path = os.path.join(data_dir, entries[0])
+        self.n_episodes = len(entries)
+        with h5py.File(h5path, 'r') as dataset_file:
+            if self.obs_type == 'state_features':
+                self.observation_dim = dataset_file['observations_features'][0].shape
+            else: # obs_type='pixel': the observation are raw pixel images
+                self.observation_dim = dataset_file['observations'][0].shape #fields.observations.shape[-1]
+            self.action_dim = dataset_file['actions'][0].shape
        
-    def process_img(img):
+    def process_img(self, img):
         return transform_img(img).numpy()
 
-    def sequence_dataset_pushv0(data_dir,
-        preprocess_fn, obs_type='state_features', max_path_length=600):
-        """
-        Returns an iterator through trajectories.
-        Args:
-            env: An OfflineEnv object.
-            data_dir: data directory
-            max_path_length: max number of steps per episode
-            dataset: An optional dataset to pass in for processing. If None,
-                the dataset will default to env.get_dataset()
-            **kwargs: Arguments to pass to env.get_dataset().
-        Returns:
-            An iterator through dictionaries with keys:
-                observations
-                actions
-                rewards
-                terminals
-        """
-        entries = os.listdir(data_dir)
-        #dataset = get_dataset(env)
-        #dataset = preprocess_fn(dataset)
-
-        for h5file in entries: # in every h5file is stored an episode 
-            h5path = os.path.join(data_dir, h5file)
-            with h5py.File(h5path, 'r') as dataset_file:
-                episode_data = {}
-                episode_length = min(max_path_length, len(dataset_file['rewards']))
-                episode_data['actions'] = dataset_file[1]['actions'][:episode_length]
-                episode_data['terminals'] = dataset_file[2]['terminals'][:episode_length]
-                episode_data['rewards'] = dataset_file[2]['rewards'][:episode_length]
-                if obs_type == 'state_fetures':
-                    episode_data['observations'] = dataset_file[0][:episode_length]
-                else: # obs_type='pixel': the observation are raw pixel images
-                    episode_data['observations'] = process_img( dataset_file[0][:episode_length] )
-                yield episode_data
-
-
-    def normalize(self, keys=['observations', 'actions']):
+    def normalize(self, keys=['actions']):
         '''
             normalize fields that will be predicted by the diffusion model
         '''
@@ -120,21 +67,11 @@ class SequenceDatasetPush(torch.utils.data.Dataset):
             normed = self.normalizer(array, key)
             self.fields[f'normed_{key}'] = normed.reshape(self.n_episodes, self.max_path_length, -1)
 
-    def make_indices(self, path_lengths, horizon):
-        '''
-            makes indices for sampling from dataset;
-            each index maps to a datapoint
-        '''
-        indices = []
-        for i, path_length in enumerate(path_lengths):
-            max_start = min(path_length - 1, self.max_path_length - horizon)
-            if not self.use_padding:
-                max_start = min(max_start, path_length - horizon)
-            for start in range(max_start):
-                end = start + horizon
-                indices.append((i, start, end))
-        indices = np.array(indices)
-        return indices
+    def normalize_actions(self, actions):
+        mean_actions = np.mean(actions)
+        std_actions = np.std(actions)
+        eps=1e-10
+        return (actions-mean_actions)/(eps+std_actions)
 
     def get_conditions(self, observations):
         '''
@@ -143,25 +80,30 @@ class SequenceDatasetPush(torch.utils.data.Dataset):
         return {0: observations[0]}
 
     def __len__(self):
-        return len(self.indices)
+        return len(self.entries)
 
-    def __getitem__(self, idx, eps=1e-4):
-        path_ind, start, end = self.indices[idx]
-
-        #if 'push' in self.env_name.lower():
-        episode =  next(self.itr())
-        observations = episode['observations']
-        actions = episode['actions']
+    def safe_batch(self):
+        return self.prev_batch
     
-        """
-        begin modified 
-        conditions = self.get_conditions(observations)
-        trajectories = np.concatenate([actions, observations], axis=-1)
-        batch = Batch(trajectories, conditions)
-        return batch
-        """
-        ## begin modifed (added lines)
-        batch = BatchV2(actions, observations[0]) # take only the first observation of the trajectory
+    def __getitem__(self, idx, eps=1e-4):
+        #path_ind, start, end = self.indices[idx]
+        h5file = self.entries[idx%self.n_episodes]
+        h5path = os.path.join(self.data_dir, h5file)
+        with h5py.File(h5path, 'r') as dataset_file:
+            episode_length = len(dataset_file['rewards'])
+            if episode_length < self.horizon:
+                print("[Using the previous batch]...")
+                return self.safe_batch()
+            path_start = np.random.randint(0, episode_length-self.horizon+1)
+            path_end = path_start + self.horizon
+            actions = self.normalize_actions(dataset_file['actions'][path_start:path_end], "actions")
+            if self.obs_type == 'state_features':
+                first_observation = dataset_file['observations_features'][path_start]
+            else: # obs_type='pixel': the observation are raw pixel images
+                first_observation = self.process_img(dataset_file["observations"][path_start])
+
+        batch = BatchV2(actions, first_observation)
+        self.prev_batch = batch
         return batch
 
 
@@ -177,7 +119,7 @@ class GoalDataset(SequenceDatasetPush):
         }
 
 
-class ValueDataset(SequenceDataset):
+class ValueDataset(SequenceDatasetPush):
     '''
         adds a value field to the datapoints for training the value function
     '''
